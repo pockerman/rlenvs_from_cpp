24ecc6c5789eb96deaeb5ef16b0e4c20e47d0683
Alex <a.giavaras@gmail.com>
2021-07-25 18:56:09 +0100

    Update tests

--- tests/test_frozen_lake/test_frozen_lake.cpp
+++ tests/test_frozen_lake/test_frozen_lake.cpp
@@ -1,12 +1,15 @@
 #include "gymfcpp/frozen_lake.h"
-#include "gymfcpp/py_interpreter.h"
+#include "gymfcpp/time_step.h"
+#include "gymfcpp/time_step_type.h"
 #include "gymfcpp/types.h"
+
 #include <gtest/gtest.h>
 #include <boost/python.hpp>
 
 namespace{
 
 using gymfcpp::uint_t;
+using gymfcpp::real_t;
 
 }
 
@@ -70,5 +73,79 @@ TEST(TestFrozenLake, Test_Make)
     {
         PyErr_Print();
         FAIL();
+    }
+}
+
+
+TEST(TestFrozenLake, Test_Reset)
+{
+
+    try{
+
+        Py_Initialize();
+
+        auto gym_module = boost::python::import("gym");
+        auto gym_namespace = gym_module.attr("__dict__");
+
+        gymfcpp::FrozenLake env("v0", gym_namespace, false);
+        env.make();
+
+        auto state = env.reset();
+        ASSERT_TRUE(state.first());
+
+    }
+    catch(const boost::python::error_already_set&)
+    {
+        PyErr_Print();
+        FAIL()<<"Error could not reset the environment";
+    }
+}
+
+TEST(TestFrozenLake, Test_Step)
+{
+
+    try{
+
+        Py_Initialize();
+
+        auto gym_module = boost::python::import("gym");
+        auto gym_namespace = gym_module.attr("__dict__");
+
+        gymfcpp::FrozenLake env("v0", gym_namespace, false);
+        env.make();
+
+        auto step_result = env.step(0);
+        ASSERT_TRUE(step_result.mid());
+
+    }
+    catch(const boost::python::error_already_set&)
+    {
+        PyErr_Print();
+        FAIL()<<"Error could not step in the environment";
+    }
+}
+
+TEST(TestFrozenLake, Test_Step_With_Query)
+{
+
+    try{
+
+        Py_Initialize();
+
+        auto gym_module = boost::python::import("gym");
+        auto gym_namespace = gym_module.attr("__dict__");
+
+        gymfcpp::FrozenLake env("v0", gym_namespace, false);
+        env.make();
+
+        auto step_result = env.step(0, true);
+        ASSERT_TRUE(step_result.mid());
+        ASSERT_DOUBLE_EQ(step_result.get_extra<real_t>("prob"), 0.3333333333333333);
+
+    }
+    catch(const boost::python::error_already_set&)
+    {
+        PyErr_Print();
+        FAIL()<<"Error could not step in the environment";
     }
 }
