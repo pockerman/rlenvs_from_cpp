293d602af256d76b0276b1d9569ef0c7ebaca917
Alex <a.giavaras@gmail.com>
2021-12-26 14:19:27 +0000

    Add constants

---
+++ src/gymfcpp/gymfcpp_consts.h
@@ -0,0 +1,30 @@
+#ifndef GYMFCPP_CONSTS_H
+#define GYMFCPP_CONSTS_H
+
+#include <string>
+
+namespace gymfcpp {
+
+///
+/// \brief PY_ENV_NAME
+///
+inline const std::string PY_ENV_NAME = "py_env_name";
+
+///
+/// \brief PY_RESET_ENV_RESULT_NAME
+///
+inline const std::string PY_RESET_ENV_RESULT_NAME = "py_reset_result_name";
+
+///
+/// \brief PY_STEP_ENV_RESULT_NAME
+///
+inline const std::string PY_STEP_ENV_RESULT_NAME = "py_step_result_name";
+
+///
+/// \brief PY_STATE_NAME
+///
+inline const std::string PY_STATE_NAME = "py_state_name";
+
+}
+
+#endif // GYMFCPP_CONSTS_H
