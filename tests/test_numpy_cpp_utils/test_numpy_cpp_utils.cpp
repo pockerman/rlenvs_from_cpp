4a058a21ba1304c7bdd50cb1bfad5c2a73d76774
Alex <a.giavaras@gmail.com>
2022-01-20 21:52:52 +0000

    #44 NOP

---
+++ tests/test_numpy_cpp_utils/test_numpy_cpp_utils.cpp
@@ -0,0 +1,86 @@
+#include "gymfcpp/numpy_cpp_utils.h"
+#include "gymfcpp/gymfcpp_types.h"
+
+
+#include <gtest/gtest.h>
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+
+namespace{
+
+using gymfcpp::uint_t;
+using gymfcpp::real_t;
+
+}
+
+TEST(TestDisigtize, MinBound) {
+
+    try{
+
+        std::vector<uint_t> bins{1, 2, 3};
+        auto bin = gymfcpp::digitize(static_cast<uint_t>(0), bins);
+        ASSERT_EQ(bin, static_cast<uint_t>(0));
+    }
+    catch(...)
+    {
+        FAIL()<<"An unknown exception occured";
+    }
+}
+
+TEST(TestDisigtize, MaxBound)
+{
+
+    try{
+
+        std::vector<uint_t> bins{1, 2, 3};
+        auto bin = gymfcpp::digitize(static_cast<uint_t>(4), bins);
+        ASSERT_EQ(bin, static_cast<uint_t>(3));
+    }
+    catch(...){
+        FAIL()<<"An unknown exception occured";
+    }
+}
+
+TEST(TestDisigtize, InBound)
+{
+
+    try{
+        std::vector<real_t> bins{1., 2., 3.};
+        auto bin = gymfcpp::digitize(1.5, bins);
+        ASSERT_EQ(bin, static_cast<uint_t>(1));
+    }
+    catch(...){
+        FAIL()<<"An unknown exception occured";
+    }
+}
+
+TEST(TestDisigtize, InBoundLowerBound)
+{
+
+    try{
+        std::vector<real_t> bins{1., 2., 3.};
+        auto bin = gymfcpp::digitize(1., bins);
+        ASSERT_EQ(bin, static_cast<uint_t>(1));
+    }
+    catch(...){
+        FAIL()<<"An unknown exception occured";
+    }
+}
+
+TEST(TestDisigtize, OutBoundMaxBound)
+{
+
+    try{
+        std::vector<real_t> bins{1., 2., 3.};
+        auto bin = gymfcpp::digitize(3., bins);
+
+        // this sould also be out of bounds
+        ASSERT_EQ(bin, static_cast<uint_t>(3));
+    }
+    catch(...){
+        FAIL()<<"An unknown exception occured";
+    }
+}
+
+
+
