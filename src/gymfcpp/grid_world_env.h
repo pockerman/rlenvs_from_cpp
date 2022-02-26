62041c2a9ff6fd8ee21d6381cf6171959331a742
Alex <a.giavaras@gmail.com>
2022-02-26 15:08:53 +0000

    #50 Update API for Gridworld

--- src/gymfcpp/grid_world_env.h
+++ src/gymfcpp/grid_world_env.h
@@ -14,11 +14,14 @@
 #include "gymfcpp/env_mixin.h"
 #include "gymfcpp/extern/enum.h"
 #include "gymfcpp/gymfcpp_config.h"
+#include "gymfcpp/time_step_type.h"
+#include "gymfcpp/numpy_cpp_utils.h"
 
 #ifdef GYMFCPP_DEBUG
 #include <cassert>
 #endif
 
+#include <vector>
 #include <string>
 #include <utility>
 #include <map>
@@ -32,6 +35,36 @@ namespace rlenvs
 
 // still we may want to use some utilities
 using namespace gymfcpp;
+
+namespace{
+
+uint_t
+max(const std::pair<uint_t,  uint_t>& position){
+    return position.first > position.second ? position.first : position.second;
+}
+
+uint_t
+min(const std::pair<uint_t,  uint_t>& position){
+    return position.first < position.second ? position.first : position.second;
+}
+
+uint_t
+get_non_zero_idx(std::pair<uint_t, uint_t>& p){
+
+}
+
+inline
+bool operator == (const std::pair<uint_t,  uint_t>& p1, const std::pair<uint_t,  uint_t>& p2){
+    return p1.first == p2.first && p1.second == p2.second;
+}
+
+inline
+std::pair<uint_t, uint_t>
+operator + (const std::pair<uint_t,  uint_t>& p1, const std::pair<uint_t,  uint_t>& p2){
+    return std::make_pair(p1.first + p2.first, p1.second + p2.second);
+}
+
+}
 
 
 ///
@@ -59,12 +92,12 @@ std::string to_string(GridworldInitType type){return type._to_string();}
 /// Player initialization means that the player is initialized at a random position on the board.
 /// Random initialization means that all the objects are placed randomly
 ///
-template<uint_t side_size>
+template<uint_t side_size_>
 class Gridworld final
 {
 public:
 
-    static_assert (side_size >= 4, "The side size should be greater than 4");
+    static_assert (side_size_ >= 4, "The side size should be greater than 4");
 
     ///
     /// \brief name
@@ -84,12 +117,15 @@ public:
     ///
     /// \brief action_space_t. The type of the action space
     ///
-    typedef DiscreteSpace<side_size * side_size> state_space_type;
+    typedef DiscreteSpace<side_size_ * side_size_> state_space_type;
 
     ///
     /// \brief state_type
     ///
-    typedef typename state_space_type::item_t state_type;
+    //typedef typename state_space_type::item_t state_type;
+
+     typedef std::vector<std::vector<std::vector<real_t>>> raw_state_type;
+     typedef std::vector<real_t> state_type;
 
     ///
     /// \brief time_step_t. The type of the time step
@@ -97,9 +133,26 @@ public:
     typedef TimeStep<state_type> time_step_type;
 
     ///
+    /// \brief n_components
+    ///
+    static const uint_t n_components = 4;
+
+    ///
+    /// \brief side_size
+    ///
+    static const uint_t side_size = side_size_;
+
+    ///
     /// \brief Constructor
     ///
     Gridworld(std::string version, GridworldInitType init_mode, bool create=true);
+
+    ///
+    /// \brief Constructor. Initializes the world by allowing noise to be
+    /// added in the observation vector
+    ///
+    Gridworld(std::string version, GridworldInitType init_mode, uint_t seed,
+              real_t noise_factor, bool create=true);
 
     ///
     /// \brief version
@@ -128,7 +181,7 @@ public:
     ///
     /// \brief n_states. Returns the number of states
     ///
-    uint_t n_states()const noexcept{ return side_size * side_size; }
+    uint_t n_states()const noexcept{ return side_size_ * side_size_; }
 
     ///
     /// \brief n_actions. Returns the number of actions
@@ -153,9 +206,39 @@ public:
     void close();
 
     ///
+    /// \brief reset the environment
     ///
+    time_step_type reset();
+
     ///
-    void reset();
+    /// \brief get_reward
+    /// \return
+    ///
+    real_t get_reward()const;
+
+    ///
+    /// \brief get_observation
+    /// \return
+    ///
+    raw_state_type get_raw_observation()const;
+
+    ///
+    /// \brief has_random_state
+    /// \return
+    ///
+    bool has_random_state()const noexcept{return randomize_state_;}
+
+    ///
+    /// \brief seed
+    /// \return
+    ///
+    uint_t seed()const noexcept{return seed_;}
+
+    ///
+    /// \brief noise_factor
+    /// \return
+    ///
+    real_t noise_factor()const noexcept{return noise_factor_;}
 
 private:
 
@@ -170,9 +253,36 @@ private:
     GridworldInitType init_mode_;
 
     ///
+    /// \brief randomize_state_
+    ///
+    bool randomize_state_;
+
+    ///
+    /// \brief seed_
+    ///
+    uint_t seed_;
+
+    ///
+    /// \brief noise_factor_
+    ///
+    real_t noise_factor_;
+
+    ///
     /// \brief is_created_
     ///
     bool is_created_;
+
+    ///
+    /// \brief The MoveType enum
+    ///
+    enum MoveType{VALID=0, INVALID=1, LOST_GAME=2};
+
+    ///
+    /// \brief The BoardComponentType enum
+    ///
+    enum BoardComponentType{PLAYER=0, GOAL=1, PIT=2, WALL=3};
+
+    typedef std::pair<uint_t,  uint_t> Position;
 
     ///
     /// \brief init_board_
@@ -199,7 +309,7 @@ private:
     /// \param row
     /// \param col
     ///
-    void check_move_(uint_t row, uint_t col)const;
+    void check_move_(uint_t row, uint_t col);
 
     ///
     /// \brief validate_move_
@@ -207,10 +317,7 @@ private:
     /// \param row
     /// \param col
     ///
-    void validate_move_(const std::string piece, uint_t row, uint_t col);
-
-
-    typedef std::pair<uint_t,  uint_t> Position;
+    MoveType validate_move_(BoardComponentType piece, Position pos);
 
     ///
     /// \brief The BoardPiece struct
@@ -284,13 +391,26 @@ private:
     };
 
     ///
-    ///
+    /// Represents the board
     ///
     struct Board
     {
         uint_t size;
-        std::map<std::string, BoardPiece> components;
+        std::map<BoardComponentType, BoardPiece> components;
         std::map<std::string, BoardMask> masks;
+
+        ///
+        /// \brief move_piece Move the pice to the given position
+        /// \param piece
+        /// \param pos
+        ///
+        void move_piece(BoardComponentType piece, Position pos);
+
+        ///
+        /// \brief get_observation
+        /// \return
+        ///
+        raw_state_type get_observation()const;
     };
 
     ///
@@ -302,17 +422,33 @@ private:
 template<uint_t side_size>
 const std::string Gridworld<side_size>::name = "Gridworld";
 
+
 template<uint_t side_size>
 Gridworld<side_size>::Gridworld(std::string version, GridworldInitType init_mode, bool create)
     :
       version_(version),
       init_mode_(init_mode),
+      randomize_state_(false),
+      seed_(0),
+      noise_factor_(0.0),
       is_created_(false)
 {
     if(create){
         make();
     }
 }
+
+template<uint_t side_size>
+Gridworld<side_size>::Gridworld(std::string version, GridworldInitType init_mode, uint_t seed,
+                                real_t noise_factor, bool create)
+    :
+      version_(version),
+      init_mode_(init_mode),
+      randomize_state_(true),
+      seed_(seed),
+      noise_factor_(noise_factor),
+      is_created_(false)
+{}
 
 template<uint_t side_size>
 void
@@ -357,14 +493,6 @@ template<uint_t side_size>
 typename Gridworld<side_size>::time_step_type
 Gridworld<side_size>::step(action_type action){
 
-    // need to determine what object (if any)
-    // is in the new grid spot the player is moving to
-    // actions in {u,d,l,r}
-    //def checkMove(addpos):
-    //        if self.validateMove('Player', addpos) in [0, 2]:
-    //            new_pos = addTuple(self.board.components['Player'].pos, addpos)
-    //            self.board.movePiece('Player', new_pos)
-
         switch( action ){
             case 0:
             {
@@ -399,20 +527,68 @@ Gridworld<side_size>::step(action_type action){
 
         }
 
+        // get the observation
+        auto obs = to_1d_from_3d<raw_state_type, real_t>(n_components, side_size, side_size, get_raw_observation());
 
+        if(has_random_state()){
 
-        /*if self.add_noise_on_state:
-            obs = self.board.render_np().reshape(1, 64) + np.random.rand(1, 64) / self.noise_factor
-        else:
-            obs = self.board.render_np().reshape(1, 64)
+            // add uniform noise and scale
+            add_uniform_noise(obs, this->seed(), noise_factor());
+        }
 
-        reward = self.get_reward()
-        step_type = StepType.LAST if reward != -1 else StepType.MID
-        time_step = TimeStep(step_type=step_type, reward=reward,
-                             info={}, observation=obs, discount=0.0)
-        return time_step*/
+        auto reward = get_reward();
+        auto step_type = reward == -1.0 ? TimeStepTp::LAST : TimeStepTp::MID;
+        auto time_step = time_step_type(step_type, reward, obs);
 
-        return time_step_type();
+        return time_step;
+}
+
+template<uint_t side_size>
+typename Gridworld<side_size>::time_step_type
+Gridworld<side_size>::reset(){
+
+    // reinitialize the board
+    init_board_();
+    auto obs = to_1d_from_3d<raw_state_type, real_t>(n_components, side_size, side_size, get_raw_observation());
+
+    if(has_random_state()){
+
+        // add uniform noise and scale
+        add_uniform_noise(obs, this->seed(), noise_factor());
+    }
+
+    auto reward = get_reward();
+    auto time_step = time_step_type(TimeStepTp::FIRST, reward, obs);
+    return time_step;
+}
+
+template<uint_t side_size>
+real_t
+Gridworld<side_size>::get_reward()const{
+
+    auto player_pos = board_.components.find(BoardComponentType::PLAYER)->second.pos;
+    auto pit_pos = board_.components.find(BoardComponentType::PIT)->second.pos;
+    auto goal_pos = board_.components.find(BoardComponentType::GOAL)->second.pos;
+    if (player_pos == pit_pos){
+        return -10.;
+    }
+    else if (player_pos == goal_pos){
+        return 10.0;
+    }
+
+    return -1.0;
+}
+
+template<uint_t side_size>
+typename Gridworld<side_size>::raw_state_type
+Gridworld<side_size>::get_raw_observation()const{
+
+#ifdef GYMFCPP_DEBUG
+    if(!is_created_){
+        assert(false && "Environment has not been created. Have you called make?");
+    }
+#endif
+    return board_.get_observation();
 }
 
 template<uint_t side_size>
@@ -429,25 +605,61 @@ Gridworld<side_size>::close(){
 
 template<uint_t side_size>
 void
-Gridworld<side_size>::reset(){
-
-}
-
-
-template<uint_t side_size>
-void
 Gridworld<side_size>::init_board_(){
 
     board_.size = side_size;
-    board_.components["Player"] = BoardPiece("Player", "P", std::make_pair(0, 0));
-    board_.components["Goal"] = BoardPiece("Goal", "G", std::make_pair(1, 0));
-    board_.components["Pit"] = BoardPiece("Pit", "-", std::make_pair(2, 0));
-    board_.components["Wall"] = BoardPiece("Wall", "W", std::make_pair(3, 0));
+    board_.components[BoardComponentType::PLAYER] = BoardPiece("Player", "P", std::make_pair(0, 0));
+    board_.components[BoardComponentType::GOAL] = BoardPiece("Goal", "G", std::make_pair(1, 0));
+    board_.components[BoardComponentType::PIT] = BoardPiece("Pit", "-", std::make_pair(2, 0));
+    board_.components[BoardComponentType::WALL] = BoardPiece("Wall", "W", std::make_pair(3, 0));
+}
+
+
+template<uint_t side_size>
+typename Gridworld<side_size>::MoveType
+Gridworld<side_size>::validate_move_(BoardComponentType piece, Position pos){
+
+    // 0 is valid
+    //auto outcome = 0 #0 is valid, 1 invalid, 2 lost game
+    auto outcome = Gridworld<side_size>::MoveType::VALID;
+
+    // get position of pit
+    auto pit_pos = board_.components[BoardComponentType::PIT].pos;
+    auto wall_pos = board_.components[BoardComponentType::WALL].pos;
+
+    auto new_pos = board_.components[piece].pos + pos;
+
+     if (new_pos == wall_pos){
+         //1 //block move, player can't move to wall
+         outcome = Gridworld<side_size>::MoveType::INVALID;
+     }
+     else if( max(new_pos) > (board_.size - 1 )){
+        // #if outside bounds of board
+         outcome = Gridworld<side_size>::MoveType::INVALID;
+    }
+    else if( min(new_pos) < 0){
+        // #if outside bounds
+         outcome = Gridworld<side_size>::MoveType::INVALID;
+    }
+    else if( new_pos == pit_pos){
+         outcome = Gridworld<side_size>::MoveType::LOST_GAME;
+    }
+
+    return outcome;
 }
 
 template<uint_t side_size>
 void
-Gridworld<side_size>::check_move_(uint_t row, uint_t col)const{
+Gridworld<side_size>::check_move_(uint_t row, uint_t col){
+
+    auto position = std::make_pair(row, col);
+    auto move_type = validate_move_(BoardComponentType::PLAYER, position);
+
+    if( move_type == MoveType::VALID || move_type == MoveType::LOST_GAME){
+
+        auto new_pos = board_.components[BoardComponentType::PLAYER].pos + position;
+        board_.move_piece(BoardComponentType::PLAYER, new_pos);
+    }
 
 }
 
@@ -456,10 +668,10 @@ void
 Gridworld<side_size>::build_static_mode_(){
 
     // Row, Column
-    board_.components["Player"].pos = std::make_pair(0,3);
-    board_.components["Goal"].pos = std::make_pair(0,0);
-    board_.components["Pit"].pos = std::make_pair(0,1);
-    board_.components["Wall"].pos = std::make_pair(1,1);
+    board_.components[BoardComponentType::PLAYER].pos = std::make_pair(0,3);
+    board_.components[BoardComponentType::GOAL].pos = std::make_pair(0,0);
+    board_.components[BoardComponentType::PIT].pos = std::make_pair(0,1);
+    board_.components[BoardComponentType::WALL].pos = std::make_pair(1,1);
 
 }
 
@@ -472,6 +684,80 @@ Gridworld<side_size>::build_random_mode_(){
 template<uint_t side_size>
 void
 Gridworld<side_size>:: build_player_mode_(){
+
+}
+
+// Board inner struct
+
+template<uint_t side_size>
+typename Gridworld<side_size>::raw_state_type
+Gridworld<side_size>::Board::get_observation()const{
+
+    // initialize the data struct the
+    // represents the state
+    auto num_pieces = components.size() + masks.size();
+    raw_state_type board(num_pieces);
+
+    for(uint_t i=0; i<num_pieces; ++i){
+        board[i].resize(size);
+
+        for(uint_t j=0; j<size; ++j){
+            board[i][j].resize(size);
+
+            for(uint_t k=0; k<size; ++k){
+                board[i][j][k] = 0;
+            }
+        }
+    }
+
+    auto layer = 0;
+
+    // check if we can move the piece
+    auto comp_begin = components.begin();
+    auto comp_end = components.end();
+
+    for(; comp_begin != comp_end; ++comp_begin){
+
+        auto position = comp_begin->second.pos;
+        board[layer][position.first][position.second] = 1;
+        layer +=1;
+    }
+
+    return board;
+
+    // initialize the data struct the
+    // represents the state
+    /* displ_board = np.zeros((num_pieces, self.size, self.size), dtype=np.uint8)
+            layer = 0
+            for name, piece in self.components.items():
+                pos = (layer,) + piece.pos
+                displ_board[pos] = 1
+                layer += 1*/
+
+
+}
+
+template<uint_t side_size>
+void
+Gridworld<side_size>::Board::move_piece(BoardComponentType piece, Position pos){
+
+    auto move = true;
+
+    // check if we can move the piece
+    auto mask_begin = masks.begin();
+    auto mask_end = masks.end();
+
+    /*for(; mask_begin != mask_end; ++mask_begin){
+        position =
+    }*/
+
+
+    //        for _, mask in self.masks.items():
+    //            if pos in zip_positions2d(mask.get_positions()):
+    //                move = False
+    if( move){
+        components[piece].pos = pos;
+    }
 
 }
 
--- tests/test_grid_world/test_grid_world.cpp
+++ tests/test_grid_world/test_grid_world.cpp
@@ -90,6 +90,7 @@ TEST(TestGridworld, TestStepValidMove)
         ASSERT_EQ(env.n_states(), static_cast<uint_t>(16));
         ASSERT_EQ(env.n_actions(), static_cast<uint_t>(4));
 
+        // this should always succeed
         env.step(1);
 
     }
@@ -99,81 +100,79 @@ TEST(TestGridworld, TestStepValidMove)
     }
 }
 
+
+TEST(TestGridworld, TestGetObservationFail)
+{
+    try{
+
+        rlenvs::Gridworld<4> env("v0", rlenvs::GridworldInitType::STATIC, false);
+
+        EXPECT_DEATH(env.get_raw_observation(), "Environment has not been created. Have you called make?");
+    }
+    catch(...)
+    {
+
+        FAIL()<<"Unknown exception thrown";
+    }
+}
+
+
+TEST(TestGridworld, TestGetObservation)
+{
+
+    try{
+
+        rlenvs::Gridworld<4> env("v0", rlenvs::GridworldInitType::STATIC, false);
+        env.make();
+
+        // make sure we have the right world
+        ASSERT_TRUE(env.is_created());
+        ASSERT_EQ(env.n_states(), static_cast<uint_t>(16));
+        ASSERT_EQ(env.n_actions(), static_cast<uint_t>(4));
+
+        auto state = env.get_raw_observation();
+
+        ASSERT_EQ(state.size(), static_cast<uint_t>(env.n_components));
+
+        for(uint_t c=0; c< env.n_components; ++c){
+            ASSERT_EQ(state[c].size(), static_cast<uint_t>(env.side_size));
+        }
+    }
+    catch(...)
+    {
+        FAIL()<<"Unknown exception thrown";
+    }
+}
+
+
+TEST(TestGridworld, TestConstructor4x4Random)
+{
+
+    try{
+
+        rlenvs::Gridworld<4> env("v0", rlenvs::GridworldInitType::STATIC, 42, 10.0, false);
+
+        ASSERT_EQ(env.n_states(), static_cast<uint_t>(16));
+        ASSERT_EQ(env.n_actions(), static_cast<uint_t>(4));
+        ASSERT_EQ(env.version(), "v0");
+        ASSERT_FALSE(env.is_created());
+        ASSERT_TRUE(env.has_random_state());
+        ASSERT_EQ(env.seed(), 42);
+        ASSERT_EQ(env.noise_factor(), 10.0);
+
+        // TODO: Think how to test this
+        ASSERT_TRUE(rlenvs::to_string(env.init_type()) == rlenvs::to_string(rlenvs::GridworldInitType::STATIC));
+        ASSERT_EQ(env.name, "Gridworld");
+
+    }
+    catch(...)
+    {
+
+        FAIL()<<"Unknown exception thrown";
+    }
+}
+
 /*
-TEST(TestFrozenLake, Test_Step)
-{
-
-    try{
-
-        Py_Initialize();
-
-        auto main_module = boost::python::import("__main__");
-        auto main_namespace = main_module.attr("__dict__");
-
-        gymfcpp::FrozenLake<4> env("v0", main_namespace, false);
-        env.make();
-
-        auto step_result = env.step(0);
-        ASSERT_TRUE(step_result.mid());
-
-    }
-    catch(const boost::python::error_already_set&)
-    {
-        PyErr_Print();
-        FAIL()<<"Error could not step in the environment";
-    }
-}
-
-TEST(TestFrozenLake, Test_Step_With_Query)
-{
-
-    try{
-
-        Py_Initialize();
-
-        auto main_module = boost::python::import("__main__");
-        auto main_namespace = main_module.attr("__dict__");
-
-        gymfcpp::FrozenLake<4> env("v0", main_namespace, false);
-        env.make();
-
-        auto step_result = env.step(0, true);
-        ASSERT_TRUE(step_result.mid());
-        ASSERT_DOUBLE_EQ(step_result.get_extra<real_t>("prob"), 0.3333333333333333);
-    }
-    catch(const boost::python::error_already_set&)
-    {
-        PyErr_Print();
-        FAIL()<<"Error could not step in the environment";
-    }
-}
-
-
-TEST(TestFrozenLake, Test_Get_Dynamics)
-{
-
-    try{
-
-        Py_Initialize();
-
-        auto main_module = boost::python::import("__main__");
-        auto main_namespace = main_module.attr("__dict__");
-
-        gymfcpp::FrozenLake<4> env("v0", main_namespace, false);
-        env.make();
-
-        auto dynamics = env.p(1, 3);
-
-        ASSERT_EQ(dynamics.size(), static_cast<uint_t>(3) );
-
-    }
-    catch(const boost::python::error_already_set&)
-    {
-        PyErr_Print();
-        FAIL()<<"Error could not step in the environment";
-    }
-}
-
 TEST(TestFrozenLake, TestRender)
 {
 
