abcefe9543eebdbfd0afbecf9caafa7fdb7848cb
Alex <a.giavaras@gmail.com>
2022-05-08 12:27:13 +0100

    #61 Add new tests, and new source files

--- src/gymfcpp/gymfcpp_consts.h
+++ src/gymfcpp/gymfcpp_consts.h
@@ -1,6 +1,7 @@
 #ifndef GYMFCPP_CONSTS_H
 #define GYMFCPP_CONSTS_H
 
+#include "gymfcpp/gymfcpp_types.h"
 #include <string>
 
 namespace gymfcpp {
@@ -24,6 +25,11 @@ inline const std::string PY_STEP_ENV_RESULT_NAME = "py_step_result_name";
 /// \brief PY_STATE_NAME
 ///
 inline const std::string PY_STATE_NAME = "py_state_name";
+
+///
+/// \brief INVALID_ID
+///
+inline const uint_t INVALID_ID = static_cast<uint_t>(-1);
 
 }
 
