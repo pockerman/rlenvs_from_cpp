97a6ea06495cc6e17bbafe47f80788926b7fabcf
Alex <a.giavaras@gmail.com>
2022-05-08 12:30:20 +0100

    #61 Add vector environment wrapper and vector time step

---
+++ src/gymfcpp/vector_time_step.h
@@ -0,0 +1,19 @@
+#ifndef VECTOR_TIME_STEP_H
+#define VECTOR_TIME_STEP_H
+
+#include "gymfcpp/gymfcpp_types.h"
+#include "gymfcpp/time_step.h"
+
+namespace rlenvs{
+
+
+template<typename StateType, typename StateAdaptor=void>
+class VectorTimeStep
+{
+
+};
+
+
+}
+
+#endif // VECTOR_TIME_STEP_H
