c5f147145e57e433869bc80212465e916cd3943e
pockerman <a.giavaras@gmail.com>
2024-01-21 18:49:11 +0000

    Add new router to API

---
+++ rest_api/api_info.py
@@ -0,0 +1,23 @@
+import gymnasium as gym
+from fastapi import APIRouter, Depends, Body, status
+from fastapi.responses import JSONResponse
+
+
+api_info_router = APIRouter(prefix="/api-info", tags=["api-info"])
+
+
+@api_info_router.get("/gymnasium")
+async  def get_has_gymnasium()->JSONResponse:
+
+    try:
+        import  gymnasium as gym
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Gymnasium found on server"})
+    except ModuleNotFoundError as e:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={"message": "Gymnasium is not installed on the server"})
+
+
+@api_info_router.get("/gymnasium/envs")
+async  def get_gymnasium_envs()->JSONResponse:
+  return JSONResponse(status_code=status.HTTP_200_OK,
+                      content={"envs": ["FrozenLake-4x4", "FrozenLake-8x8", "Taxi-v3"]})
