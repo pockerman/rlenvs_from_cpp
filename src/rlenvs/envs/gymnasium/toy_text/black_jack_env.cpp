576e00f36c839adea62f26ef8a595bb01211fc15
pockerman <a.giavaras@gmail.com>
2024-02-04 11:17:24 +0000

    Add C++ BlackJack environment (#84)

---
+++ src/rlenvs/envs/gymnasium/toy_text/black_jack_env.cpp
@@ -0,0 +1,118 @@
+#include "rlenvs/envs/gymnasium/toy_text/black_jack_env.h"
+#include "rlenvs/extern/nlohmann/json/json.hpp"
+#include "rlenvs/rlenvscpp_config.h"
+
+#ifdef RLENVSCPP_DEBUG
+#include <cassert>
+#endif
+
+
+namespace rlenvs_cpp{
+namespace envs{
+namespace gymnasium
+{
+
+const std::string BlackJackData::name = "BlackJack";
+
+
+BlackJack::dynamics_t
+BlackJack::build_dynamics_from_response_(const http::Response& response)const{
+   throw std::logic_error("Environment BlackJack does not have dynamics");
+}
+
+BlackJack::time_step_type
+BlackJack::create_time_step_from_response_(const http::Response& response)const{
+
+    auto str_response = std::string(response.body.begin(), response.body.end());
+    using json = nlohmann::json;
+
+    json j = json::parse(str_response);
+
+    auto step_type = j["time_step"]["step_type"];
+    auto reward = j["time_step"]["reward"];
+    auto discount = j["time_step"]["discount"];
+    auto observation = j["time_step"]["observation"];
+    auto info = j["time_step"]["info"];
+    return BlackJack::time_step_type(time_step_type_from_int(step_type),
+                                                 reward, observation, discount,
+                                                 std::unordered_map<std::string, std::any>());
+}
+
+BlackJack::BlackJack(const std::string& api_base_url)
+    :
+   ToyTextEnvBase<BlackJackData::time_step_type>(api_base_url + "/black-jack-env")
+{}
+
+
+
+void
+BlackJack::make(const std::string& version,
+                      const std::unordered_map<std::string, std::any>& options){
+
+     if(this->is_created()){
+        return;
+    }
+
+    auto natural_itr = options.find("natural");
+    if( natural_itr != options.end()){
+        is_natural_ = std::any_cast<bool>(natural_itr->second);
+    }
+
+    auto sab_itr = options.find("sab");
+    if( sab_itr != options.end()){
+        is_sab_ = std::any_cast<bool>(sab_itr->second);
+    }
+
+    const auto request_url = std::string(this->get_url()) + "/make";
+    http::Request request{request_url};
+
+    using json = nlohmann::json;
+    json j;
+    j["version"] = version;
+    j["natural"]  = is_natural_;
+    j["sab"] = is_sab_;
+
+    auto body = j.dump();
+    const auto response = request.send("POST", body);
+
+    if(response.status.code != 201){
+        throw std::runtime_error("Environment server failed to create Environment");
+    }
+
+    this->set_version(version);
+    this->make_created();
+
+}
+
+BlackJack::time_step_type
+BlackJack::step(BlackJackActionsEnum action){
+
+    #ifdef RLENVSCPP_DEBUG
+     assert(this->is_created_ && "Environment has not been created");
+#endif
+
+     if(this->get_current_time_step_().last()){
+         return this->reset(42);
+     }
+
+    const auto request_url = std::string(this->get_url()) + "/step";
+    http::Request request{request_url};
+
+    auto body = std::to_string(action);
+    const auto response = request.send("POST", body);
+
+    if(response.status.code != 202){
+        throw std::runtime_error("Environment server failed to step environment");
+    }
+
+    this->get_current_time_step_() = this->create_time_step_from_response_(response);
+    return this->get_current_time_step_();
+
+
+
+}
+
+
+}
+}
+}
---
+++ src/rlenvs/envs/gymnasium/toy_text/black_jack_env.h
@@ -0,0 +1,208 @@
+#ifndef BLACK_JACK_H
+#define BLACK_JACK_H
+
+/**
+ *  BlackJack environment
+ *  https://github.com/Farama-Foundation/Gymnasium/blob/main/gymnasium/envs/toy_text/blackjack.py
+ *
+ *
+ *
+ *
+ */
+
+#include "rlenvs/rlenvs_types_v2.h"
+#include "rlenvs/time_step.h"
+#include "rlenvs/discrete_space.h"
+#include "rlenvs/extern/HTTPRequest.hpp"
+#include "rlenvs/envs/gymnasium/toy_text/toy_text_base.h"
+
+#include <string>
+#include <vector>
+#include <tuple>
+
+namespace rlenvs_cpp{
+namespace envs{
+namespace gymnasium {
+
+
+BETTER_ENUM(BlackJackActionsEnum, int, STICK=0, HIT=1, INVALID_ACTION=2);
+
+///
+/// \brief The CliffWorldData struct
+///
+struct BlackJackData
+{
+    ///
+    /// \brief action_space_t. The type of the action space
+    ///
+    typedef DiscreteSpace<2> action_space_type;
+
+    ///
+    /// \brief action_t
+    ///
+    typedef action_space_type::item_t action_type;
+
+    ///
+    /// \brief state_space_type
+    ///
+    typedef DiscreteSpace<48> state_space_type;
+
+    ///
+    /// \brief state_type
+    ///
+    typedef std::vector<std::tuple<uint_t, uint_t, uint_t> > state_type;
+
+
+    ///
+    /// \brief name
+    ///
+    static  const std::string name;
+
+    ///
+    /// \brief time_step_t. The type of the time step
+    ///
+    typedef TimeStep<state_type> time_step_type;
+
+};
+
+///
+/// \brief The BlackJack class. Wrapper to the Blackjack
+/// OpenAI-Gym environment.
+///
+class BlackJack:  public ToyTextEnvBase<BlackJackData::time_step_type>
+{
+
+public:
+
+    ///
+    /// \brief dynamics_t
+    ///
+    typedef std::vector<std::tuple<real_t, uint_t, real_t, bool>> dynamics_type;
+
+    ///
+    /// \brief env_data_t
+    ///
+    typedef BlackJackData  env_data_type;
+
+    ///
+    /// \brief action_space_t. The type of the action space
+    ///
+    typedef BlackJackData::action_space_type action_space_type;
+
+    ///
+    /// \brief action_t
+    ///
+    typedef BlackJackData::action_type action_type;
+
+    ///
+    /// \brief state_space_t
+    ///
+    typedef BlackJackData::state_space_type state_space_type;
+
+    ///
+    /// \brief state_t
+    ///
+    typedef BlackJackData::state_type state_type;
+
+    ///
+    /// \brief time_step_t. The type of the time step
+    ///
+    typedef BlackJackData::time_step_type time_step_type;
+
+    ///
+    /// \brief action_t
+    ///
+    typedef uint_t action_t;
+
+    ///
+    /// \brief state_t
+    ///
+    typedef uint_t state_t;
+
+    ///
+    /// \brief time_step_t
+    ///
+    typedef TimeStep<state_t> time_step_t;
+
+    ///
+    /// \brief BlackJack. Constructor.
+    ///
+    BlackJack(const std::string& api_base_url);
+
+    ///
+    /// \brief ~BlackJack. Destructor
+    ///
+    ~BlackJack()=default;
+
+   ///
+    /// \brief make. Builds the environment. Optionally we can choose if the
+    /// environment will be slippery
+    ///
+    virtual void make(const std::string& version,
+                      const std::unordered_map<std::string, std::any>& options) override final;
+
+    ///
+    /// \brief n_states
+    ///
+    uint_t n_states()const{return state_space_type::size;}
+
+    ///
+    /// \brief n_actions
+    /// \return
+    ///
+    uint_t n_actions()const noexcept{return action_space_type::size; }
+
+    ///
+    /// \brief step
+    /// \param action
+    /// \return
+    ///
+    time_step_type step(BlackJackActionsEnum action);
+
+    ///
+    ///
+    ///
+    bool is_natural()const noexcept{return is_natural_;}
+
+    ///
+    ///
+    ///
+    bool is_sab()const noexcept{return is_sab_;}
+
+
+
+protected:
+
+    ///
+    /// \brief build the dynamics from response
+    ///
+    virtual dynamics_t build_dynamics_from_response_(const http::Response& response)const override final;
+
+    ///
+    /// \brief Handle the reset response from the environment server
+    ///
+    virtual time_step_type create_time_step_from_response_(const http::Response& response) const override final;
+
+
+
+private:
+
+    ///
+    /// \brief Flag indicating if the environment has been
+    /// initialized as natural
+    ///
+    bool is_natural_{false};
+
+
+    bool is_sab_{false};
+
+    void make_natural()noexcept{is_natural_ = true;}
+
+    void make_sab()noexcept{is_sab_ = true;}
+
+};
+
+}
+}
+}
+#endif // BLACK_JACK_H
