7e550c0b1f92f7dad174276c8a062886f065e5fa
pockerman <a.giavaras@gmail.com>
2024-02-10 19:31:24 +0000

    Add cart pole environment (#83)

---
+++ rest_api/classic_control/cart_pole_env_api.py
@@ -0,0 +1,114 @@
+import gymnasium as gym
+from typing import Union
+from fastapi import APIRouter, Depends, Body, status
+from fastapi.responses import JSONResponse
+from fastapi import HTTPException
+from time_step_response import TimeStep, TimeStepType
+
+cart_pole_router = APIRouter(prefix="/cart-pole-env", tags=["cart-pole-env"])
+
+# the environment to create
+env = None
+ENV_NAME = "CartPole"
+
+# actions that the environment accepts
+ACTIONS_SPACE = {0: "Push cart to the left", 1: "Push cart to the right"}
+
+
+@cart_pole_router.get("/action-space")
+async def get_action_space() -> JSONResponse:
+    return JSONResponse(status_code=status.HTTP_200_OK,
+                        content={"action_space": ACTIONS_SPACE})
+
+
+@cart_pole_router.get("/is-alive")
+async def get_is_alive() -> JSONResponse:
+    global env
+
+    if env is None:
+        return JSONResponse(status_code=status.HTTP_200_OK,
+                            content={"result": False})
+    else:
+        return JSONResponse(status_code=status.HTTP_200_OK,
+                            content={"result": True})
+
+
+@cart_pole_router.post("/make")
+async def make(version: str = Body(default="v1"), natural: bool = Body(default=False),
+               sab: bool = Body(default=False)) -> JSONResponse:
+    global env
+    if env is not None:
+        env.close()
+
+    try:
+        env = gym.make(f"{ENV_NAME}-{version}", natural, sab)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=str(e))
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED,
+                        content={"result": True})
+
+
+@cart_pole_router.post("/close")
+async def close() -> JSONResponse:
+    global env
+
+    if env is not None:
+        env.close()
+        return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
+                            content={"message": f"Environment {ENV_NAME} is closed"})
+
+    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                        content={"message": f"Environment {ENV_NAME} has not been created"})
+
+
+@cart_pole_router.post("/reset")
+async def reset(seed: int = Body(default=42)) -> JSONResponse:
+    """Reset the environment
+
+    :return:
+    """
+
+    global env
+
+    if env is not None:
+        observation, info = env.reset(seed=seed)
+
+        step = TimeStep(observation=observation,
+                        reward=0.0,
+                        step_type=TimeStepType.FIRST,
+                        info=info,
+                        discount=1.0)
+        return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
+                            content={"time_step": step.model_dump()})
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail={"message": f"Environment {ENV_NAME} is not initialized."
+                                           " Have you called make()?"})
+
+
+@cart_pole_router.post("/step")
+async def step(action: int = Body(...)) -> JSONResponse:
+    global env
+
+    if action not in ACTIONS_SPACE:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Action {action} not in {list(ACTIONS_SPACE.keys())}")
+
+    if env is not None:
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        step = TimeStep(observation=observation,
+                        reward=reward,
+                        step_type=TimeStepType.MID if not terminated  else TimeStepType.LAST,
+                        info=info,
+                        discount=1.0)
+
+        return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
+                            content={"time_step": step.model_dump()})
+
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail=f"Environment {ENV_NAME} is not initialized. Have you called make()?")
+
+
