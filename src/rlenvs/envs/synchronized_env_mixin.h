e1d8e6e801dc801d48d72947a24da7647eaf511e
pockerman <a.giavaras@gmail.com>
2024-05-19 11:22:01 +0100

    Add synchronized_env_mixin struct (#92)

---
+++ src/rlenvs/envs/synchronized_env_mixin.h
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2024 <copyright holder> <email>
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef SYNCHRONIZED_ENV_MIXIN_H
+#define SYNCHRONIZED_ENV_MIXIN_H
+
+
+#include <any>
+#include <string>
+#include <unordered_map>
+
+namespace rlenvs_cpp{
+namespace envs{
+
+/**
+ * @brief class synchronized_env_mixin
+ */
+struct synchronized_env_mixin
+{
+    /**
+     * @brief Synchronize the environment
+     *
+     */
+    virtual void sync(const std::unordered_map<std::string, std::any>& options){}
+};
+
+}
+}
+#endif // SYNCHRONIZED_ENV_MIXIN_H
