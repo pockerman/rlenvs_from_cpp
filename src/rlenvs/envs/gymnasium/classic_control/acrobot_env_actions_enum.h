41efcf707b13b3815635a60ff16c8a245016bd29
pockerman <a.giavaras@gmail.com>
2024-12-01 11:16:17 +0000

    Refactor acrobot actions enum (#107)

---
+++ src/rlenvs/envs/gymnasium/classic_control/acrobot_env_actions_enum.h
@@ -0,0 +1,32 @@
+#ifndef ACROBOT_ENV_ACTIONS_ENUM_H
+#define ACROBOT_ENV_ACTIONS_ENUM_H
+
+#include "rlenvs/extern/enum.h"
+#include "rlenvs/rlenvs_types_v2.h"
+
+#include <vector>
+namespace rlenvs_cpp{
+namespace envs{
+namespace gymnasium{
+	
+	BETTER_ENUM(AcrobotActionsEnum, int, ZERO=0, ONE=1, TWO=2, INVALID_ACTION=3);
+	
+	
+	/**
+     * @brief Convert the action index to a valid AcrobotActionsEnum
+     *
+     * */
+	struct AcrobotActionsEnumUtils{
+		static AcrobotActionsEnum action_from_int(uint_t aidx);
+		static std::vector<AcrobotActionsEnum> actions_from_ints(const std::vector<uint_t> actions);
+		static std::vector<int> actions_to_ints(const std::vector<AcrobotActionsEnum>& actions);
+		static int action_to_int(const AcrobotActionsEnum action);
+	};
+    
+}
+	
+}
+	
+}
+
+#endif
