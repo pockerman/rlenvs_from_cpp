8824a520bd5892dee601c0820ce8966544926087
pockerman <a.giavaras@gmail.com>
2024-12-23 17:22:07 +0000

    Add with_rest_api_mixin class (#111)

---
+++ src/rlenvs/envs/with_rest_api_mixin.h
@@ -0,0 +1,73 @@
+#ifndef WITH_REST_API_MIXIN_H
+#define WITH_REST_API_MIXIN_H
+
+#include "rlenvs/extern/HTTPRequest.hpp"
+#include <string>
+
+namespace rlenvs_cpp{
+namespace envs{
+	
+	
+///
+/// \brief class synchronized_env_mixin
+///
+template<typename TimeStepType>
+class with_rest_api_mixin
+{
+	
+	typedef TimeStepType time_step_type;
+	
+	///
+	/// \brief Constructor 
+	///
+	with_rest_api_mixin(const std::string& api_url, const std::string& resource_path);
+	
+	///
+	/// \brief destructor
+	///
+	virtual ~with_rest_api_mixin()=default;
+	
+    ///
+	/// \brief Returns the url the environment is using
+	///
+    std::string get_url()const noexcept{return api_url_ + resource_path_;}
+
+    ///
+	/// \brief Returns the url the environment is using
+	///
+    std::string get_api_url()const noexcept{return api_url_;}
+	
+	///
+	/// \brief The resource path
+	///
+	std::string get_resource_path()const noexcept {return resource_path_;}
+	
+	///
+    /// \brief build the time step from the server response
+    ///
+    virtual time_step_type create_time_step_from_response_(const http::Response& response)const=0;
+	
+	///
+    /// \brief Query the environment server is the environment has been created
+    ///
+    virtual bool is_alive()const=0;
+	
+private:
+	
+	std::string api_url_;
+	std::string resource_path_;
+	
+		
+};
+
+template<typename TimeStepType>
+with_rest_api_mixin<TimeStepType>::with_rest_api_mixin(const std::string& api_url, const std::string& resource_path)
+:
+api_url_(api_url),
+resource_path_(resource_path)
+{}
+	
+}
+}
+
+#endif 
