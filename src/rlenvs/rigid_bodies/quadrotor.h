9ea24dc1d145a602acf49c3e78236d40877d8d0d
pockerman <a.giavaras@gmail.com>
2024-12-28 15:11:15 +0000

    Add rigid_bodies directory (#112)

---
+++ src/rlenvs/rigid_bodies/quadrotor.h
@@ -0,0 +1,37 @@
+#ifndef QUADROTOR_H
+#define QUADROTOR_H
+
+#include "rlenvs/rlenvs_types_v2.h"
+
+namespace rlenvscpp{
+namespace rigid_bodies{
+
+///
+///
+class Quadrotor
+{
+public:
+	
+	Quadrotor
+
+private:
+
+	///
+	/// \brief The mass of the quadrotor
+	///
+	real_t mass_;	
+	
+	///
+	/// \brief the arm length
+	///
+	real_t l_;
+		
+	
+};	
+	
+}
+}
+
+
+
+#endif
